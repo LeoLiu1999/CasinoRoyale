8278bc117b41c342b1a028aea81fd776b67505c6
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-01-11 08:19:06 -0500

    MVP Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,18 @@
 public class Game{
-    final String[] faces = {"s","h","c","d"};
+    final String[] suits = {"s","h","c","d"};
     final int[] nums = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 
     private String[] _deck = new String[52];
     private double _bet;
     private double _winnings;
+
+    // protected void printDeck(){
+    // 	String ret = "";
+    // 	for (String s : _deck){
+    // 	    ret += s + " ";
+    // 	}
+    // 	System.out.println(ret);
+    // }
 
     protected void swap(int a, int b){
         String c = _deck[a];
@@ -20,19 +28,19 @@ public class Game{
 
     protected void createDeck(){
         int x = 0;
-	for(String s:faces){
+	for(String s:suits){
 	    for(int i:nums){
 		_deck[x] = s + i;
 		x += 1;
 	    }
 	}
-
-        
+	shuffle();
     }
     
-    public static void main(String[] args){
-	Game g = new Game();
+    // public static void main(String[] args){
+    // 	Game g = new Game();
 
-	g.createDeck();
-    }
+    // 	g.createDeck();
+    // 	g.printDeck();
+    // }
 }

67c0966b79c9d326cbf98cda80ccc6239f7a8d95
Michael Ruvinshteyn <Michael Ruvinshteyn>
2017-01-12 18:14:20 -0500

    type checker added

---
+++ Typetest.java
@@ -0,0 +1,11 @@
+public class Typetest{
+    public String check(double d){
+	return "double";
+    }
+    public String check(int i){
+	return "int";
+    }
+    public String check(String s){
+	return "String";
+    }
+}

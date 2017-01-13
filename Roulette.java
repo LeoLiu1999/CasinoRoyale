dd1a4a20c034e7ea46b8d31dfe4bb4506294a51d
TakingTheL <yliu6@stuy.edu>
2017-01-12 20:04:55 -0500

    finished Roulette MVP

--- Roulette.java
+++ Roulette.java
@@ -1,11 +1,13 @@
 import cs1.Keyboard;
 
 public class Roulette extends Game{
-    private final String[] _wheel = {"g0","g00", "r1","b2","r3","b4","r5","b6","r7","b8","r9","b10","b11","r12","b13","r14","b15","r16","b17","r18","r19","b20","r21","b22","r23","b24","r25","b26","r27","b28","b29","r30","b31","r32","b33","r34","b35","r36"};
+    private static final String[] _wheel = {"g0","g00", "r1","b2","r3","b4","r5","b6","r7","b8","r9","b10","b11","r12","b13","r14","b15","r16","b17","r18","r19","b20","r21","b22","r23","b24","r25","b26","r27","b28","b29","r30","b31","r32","b33","r34","b35","r36"};
     private String USERCOLORBET;
     private String USERNUMBET;
+
+
     
-    public String spin(){
+    public static String spin(){
 	return _wheel[(int)(Math.random() * 38)];
     }
 
@@ -13,15 +15,17 @@ public class Roulette extends Game{
     //	return _bets;
     //}
 
-    public boolean winColor(){
-	return (USERCOLORBET.equals(spin().substring(0,1)));
+    public static boolean winColor(String color){
+	return (color.equals(spin().substring(0,1)));
     }
 
-    public boolean winNumber(){
-	return (USERNUMBET.equals(spin().substring(1)));
+    public static boolean winNumber(String num){
+	return (num.equals(spin().substring(1)));
     }
     
     public static void main(String[] args){
+	Roulette wheel1 = new Roulette();
+	
 	//double bet = setBet();
 	int numOrColor = 0;
 	while (numOrColor != 2 && numOrColor != 1){
@@ -35,7 +39,7 @@ public class Roulette extends Game{
 	if (numOrColor == 1){
 	    System.out.println("Which number would you like to bet on? 00, 0, 1, 2, ..., 36" );
 	    USERNUMBET = Keyboard.readString();
-	    if (winNumber()){
+	    if (winNumber( USERNUMBET)){
 		System.out.println("You won " +"ENTER BET" + " dollars!" );
 	    } else {
 		System.out.println("You lost...");
@@ -48,6 +52,11 @@ public class Roulette extends Game{
 	if (numOrColor == 2){
 	    System.out.println("Would you like to bet on red(r), black(b), or green(g)");
 	    USERCOLORBET = Keyboard.readString();
+	    if (winColor(USERCOLORBET)){
+		System.out.println("You won " + "ENTER BET AMOUNT HERE" + " dollars!");
+	    } else {
+		System.out.println("You lost...");
+	    }
 	    
 	}
     }

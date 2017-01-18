52b2f0b309a0ee74666c980fad4e9c8dc4874c09
Andrew Qu <aqu3@stuy.edu>
2017-01-18 07:23:10 -0500

    Roulette asking for bet twice bugfix

--- Woo.java
+++ Woo.java
@@ -73,12 +73,12 @@ public class Woo{
 			System.out.println("Would you like to bet on number(1) or color(2)?");
 			numOrColor = Keyboard.readInt();
 		    }
-		    double betAmt = 0.00;
-		    while (!(betAmt > 0)){
-			System.out.print("Please input how much money you would like to bet: ");
-			betAmt = Keyboard.readDouble();
-			System.out.println();
-		    }
+		    double betAmt;
+		    //while (!(betAmt > 0)){
+		    //	System.out.print("Please input how much money you would like to bet: ");
+		    //	betAmt = Keyboard.readDouble();
+		    //	System.out.println();
+		    //}
 
 		    //User bets on a NUMBER
 		    //==================================
@@ -92,11 +92,9 @@ public class Woo{
 			    betAmt = Keyboard.readDouble();
 			}
 			if (Roulette.winNumber( USERNUMBET)){
-			    System.out.println("You won " + betAmt + " dollars!" );
 			    player.win(betAmt);
 			}
 			else {
-			    System.out.println("You lost...");
 			    player.lose(betAmt);
 			}
 		    }
@@ -113,11 +111,17 @@ public class Woo{
 			    System.out.print("I'm sorry, that value is invalid. Please try again: ");
 			    choice = Keyboard.readString();
 			}
+
+			betAmt = 0.00;
+			while (!(betAmt > 0)){
+			    System.out.println("Please input how much money you would like to bet");
+			    betAmt = Keyboard.readDouble();
+			}
 			
 			if (Roulette.winColor(USERCOLORBET)){
-			    System.out.println("You won " + betAmt + " dollars!");
+			    player.win(betAmt);
 			} else {
-			    System.out.println("You lost...");
+			    player.lose(betAmt);
 			}
 		    }
 		}

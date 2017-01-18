34cc55c758573a9043ff4f6800e34fb0bfd283f7
Michael Ruvinshteyn <Michael Ruvinshteyn>
2017-01-17 20:57:17 -0500

    Solved Leo's issue

--- Woo.java
+++ Woo.java
@@ -73,6 +73,11 @@ public class Woo{
 			System.out.println("Would you like to bet on number(1) or color(2)?");
 			numOrColor = Keyboard.readInt();
 		    }
+		    double betAmt = 0.00;
+		    while (!(betAmt > 0)){
+			System.out.println("Please input how much money you would like to bet");
+			betAmt = Keyboard.readDouble();
+		    }
 
 		    //User bets on a NUMBER
 		    //==================================
@@ -80,8 +85,13 @@ public class Woo{
 		    if (numOrColor == 1){
 			System.out.println("Which number would you like to bet on? 00, 0, 1, 2, ..., 36" );
 			USERNUMBET = Keyboard.readString();
+			betAmt = 0.00;
+			while (!(betAmt > 0)){
+			    System.out.println("Please input how much money you would like to bet");
+			    betAmt = Keyboard.readDouble();
+			}
 			if (Roulette.winNumber( USERNUMBET)){
-			    System.out.println("You won " +"ENTER BET AMOUNT HERE" + " dollars!" );
+			    System.out.println("You won " + betAmt + " dollars!" );
 			} else {
 			    System.out.println("You lost...");
 			}

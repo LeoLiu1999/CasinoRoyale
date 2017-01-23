7e6070f695ba32b2df388f6431231b04782b37a1
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-01-23 02:09:37 -0500

    Fixed end-of-the-day errors

--- Roulette.java
+++ Roulette.java
@@ -1,6 +1,6 @@
 import cs1.Keyboard;
 
-public class Roulette extends Game{
+public class Roulette{
     private static final String[] _wheel = {"g0","g00", "r1","b2","r3","b4","r5","b6","r7","b8","r9","b10","b11","r12","b13","r14","b15","r16","b17","r18","r19","b20","r21","b22","r23","b24","r25","b26","r27","b28","b29","r30","b31","r32","b33","r34","b35","r36"};
     private String USERCOLORBET;
     private String USERNUMBET;
--- Slots.java
+++ Slots.java
@@ -1,11 +1,17 @@
 import cs1.Keyboard;
 
-public class Slots extends Game{
+public class Slots{
 
     private String[] slot1 = {"peach","pear","cherry","grapefruit","orange","pomegranate"};
     private String[] slot2 = {"peach","pear","cherry","grapefruit","orange","pomegranate"};
     private String[] slot3 = {"peach","pear","cherry","grapefruit","orange","pomegranate"};
 
+    public void swap(String[] s, int a, int b){
+        String c = s[a];
+	s[a] = s[b];
+	s[b] = c;
+    }
+    
     public void spin(){
 	for (int x = 0; x < 1000; x += 1){
 	    swap(slot1, (int)(Math.random() * 6), (int)(Math.random() * 6));

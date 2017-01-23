bc9555b92bf1e74e14046885a115ac51c22d2bc4
Michael Ruvinshteyn <Michael Ruvinshteyn>
2017-01-22 22:37:11 -0500

    Comments added to Woo

--- Woo.java
+++ Woo.java
@@ -2,43 +2,43 @@ import cs1.Keyboard;
 
 public class Woo{
     //HELPERS----------------------------------------------------------------------------------
-    public static boolean checkInString(String[] s, String x){
+    public static boolean checkInString(String[] s, String x){ //Checks if the given String array contains the given String
 	for (String p:s){
 	    if (p.equals(x)){
-		return true;
+		return true; //breaks loop and returns true if String was found
 	    }
 	}
-	return false;
+	return false; //returns false if no instance of the String was found
     }
-    public static double getValidBet(Player player,double bet){
+    public static double getValidBet(Player player,double bet){ //Makes sure user input is a valid numerical value when asked for bets
 	System.out.println();
 	double num = bet;
-	while (num > player.checkBal() || num <= 0 || Double.isNaN(num)){
-	       	if (Double.isNaN(num)){ //checks if input was "not a number"
+	while (num > player.checkBal() || num <= 0 || Double.isNaN(num)){ //Bad cases: bet <=0, bet is more than the user has, bet is not a numerical value
+	       	if (Double.isNaN(num)){
        		    System.out.print("Please enter a numerical value: $");
        		    num = Keyboard.readDouble();
        		}
-       		if (num > player.checkBal()){ //cannot bet more than you have
+       		if (num > player.checkBal()){
        		    System.out.print("Please enter a value that is less than or equal to your balance: $");
        		    num = Keyboard.readDouble();
        		}
-       		if (num <= 0){ //cannot bet negative value
+       		if (num <= 0){
        		    System.out.print("Please enter a value above 0: $");
        		    num = Keyboard.readDouble();
        		}
        	}
-	return num;
+	return num; //returns the bet once the qualities above are satisfied
     }
     //-----------------------------------------------------------------------------------------
 
     public static void main(String[] args){
 
 	System.out.print("Welcome to the Casino Royale! What is your name? ");
-	String playerName = Keyboard.readString();
+	String playerName = Keyboard.readString(); //Asks user for their name
 	System.out.println();
 	System.out.print("Please enter your current balance, " + playerName + ": $");
-	double playerBal = Keyboard.readDouble();
-        while (playerBal <= 0 || Double.isNaN(playerBal)){
+	double playerBal = Keyboard.readDouble(); //Asks user for their starting balance
+        while (playerBal <= 0 || Double.isNaN(playerBal)){ //Bad cases: balance <=0 or balance is not a numerical input
 	    if (Double.isNaN(playerBal)){ //checks if user input was "not a number"
 		System.out.print("Please enter a numerical value: $");
 		playerBal = Keyboard.readDouble();
@@ -49,11 +49,11 @@ public class Woo{
 	    }
 	}
 
-	Player player = new Player(playerName, playerBal);
+	Player player = new Player(playerName, playerBal); //Creates new instance of Player based on given inputs
 
 	System.out.println();
 	
-	while (true){
+	while (true){ //Infinite loop until user decides to exit (choice 4)
 	    System.out.println("CURRENT BALANCE: $" + player.checkBal());
 	    System.out.println();
 	    System.out.println("Which game would you like to play?");
@@ -66,9 +66,8 @@ public class Woo{
 	    String choice = Keyboard.readString();
 
 	    boolean playAgain = true;
-	    while(playAgain){
-		//check that user input was 1 or 2 or 3 or 4
-		while((!choice.equals("1")) && (!choice.equals("2")) && (!choice.equals("3")) && (!choice.equals("4"))){
+	    while(playAgain){ //Valid until user chooses choice 4
+		while((!choice.equals("1")) && (!choice.equals("2")) && (!choice.equals("3")) && (!choice.equals("4"))){ //Bad cases: input is not 1, 2, 3, or 4
 		    System.out.print("I'm sorry, that value is invalid. Please try again: ");
 		    choice = Keyboard.readString();
 		}
@@ -81,21 +80,21 @@ public class Woo{
 		    Slots machine = new Slots();
 		
 		    System.out.println("Welcome to our state-of-the-art Slot Machine!");
-		    System.out.print("How much would you like to bet? $:");
-		    double numBet = getValidBet(player,Keyboard.readDouble());
+		    System.out.print("How much would you like to bet? $");
+		    double numBet = getValidBet(player,Keyboard.readDouble()); //asks user for VALID bet
 
 		    machine.spin();
 
 	
 		    if (machine.jackpot()){
 		        System.out.println("JACKPOT");
-			player.win(numBet * 10);
+			player.win(numBet * 10); //Player wins 10* what they bet when they get a jackpot
 		    }
 		    else if(machine.miniWin()){
-		        player.win(numBet);
+		        player.win(numBet); //Player wins what they bet when they get a miniwin
 		    } 
 		    else{
-			player.lose(numBet);
+			player.lose(numBet); //Player loses what they bet otherwise
 		    }
 		}
 		/*============================================
@@ -109,32 +108,30 @@ public class Woo{
 		  Roulette
 		  ============================================*/
 		if (choice.equals("2")){ 
-	    
-		    //double bet = setBet();
-		    int numOrColor = 0;
-		    while (numOrColor != 2 && numOrColor != 1){ //will ask until valid choice is inputted
+		    String numOrColorIn = "";
+		    while (!numOrColorIn.equals("1") && !numOrColorIn.equals("2")){ //Bad cases: input is not 1 or 2
 			System.out.print("Would you like to bet on number(1) or color(2)? ");
-			numOrColor = Keyboard.readInt();
+			numOrColorIn = Keyboard.readString();
 		    }
+		    
+		    int numOrColor = Integer.parseInt(numOrColorIn);
 
 		    //User bets on a NUMBER
 		    //==================================
 		    String USERNUMBET = "";
-		    String[] numbers = {"00","0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36"};
+		    String[] numbers = {"00","0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36"};  //Collection of possible numbers for when the user bets on a number
 		    if (numOrColor == 1){
 			System.out.print("Which number would you like to bet on? 00, 0, 1, 2, ..., 36: " );
 			USERNUMBET = Keyboard.readString();
-			while (!checkInString(numbers,USERNUMBET)){
+			while (!checkInString(numbers,USERNUMBET)){ //While the input is not one of the numbers in the array above
 			    System.out.print("That input was invalid, please try again: ");
-			    USERNUMBET = Keyboard.readString();
+			    USERNUMBET = Keyboard.readString(); //Ask for the input again
 			}
 			System.out.print("Please enter how much you would like to bet: $");
-			double numBet = getValidBet(player,Keyboard.readDouble());
+			double numBet = getValidBet(player,Keyboard.readDouble()); //asks user for VALID bet
 
 			if (Roulette.winNumber(USERNUMBET)){
-
-			    System.out.println("You won " + numBet * 35 + " dollars!" );
-			    player.win(numBet * 34);
+			    player.win(numBet * 34); //Player wins more if they bet on a number (smaller chances)
 			}
 			else {
 			    player.lose(numBet);
@@ -155,33 +152,17 @@ public class Woo{
 			}
 
 			System.out.print("Please enter how much you would like to bet: $");
-			double numBet = Keyboard.readDouble();
-		        while (numBet > player.checkBal() || numBet <= 0 || Double.isNaN(numBet)){
-			    if (Double.isNaN(numBet)){
-				System.out.print("Please enter a numerical value: $");
-				numBet = Keyboard.readDouble();
-			    }
-			    if (numBet > player.checkBal()){
-				System.out.print("Please enter a value that is less than or equal to your balance: $");
-				numBet = Keyboard.readDouble();
-			    }
-			    if (numBet <= 0){
-				System.out.print("Please enter a value above 0: $");
-				numBet = Keyboard.readDouble();
-			    }
-			}
-			
+		        double numBet = getValidBet(player,Keyboard.readDouble()); //asks user for VALID bet
 			
 			if (Roulette.winColor(USERCOLORBET)){
-			    player.win(numBet);
-			    if(USERCOLORBET.equals("g")){
-				System.out.println("You won " + numBet * 17 + " dollars!");
+			    if(USERCOLORBET.equals("g")){ //in the case where a user wins on GREEN
 				player.win(numBet * 17);
-			    } else {
-				System.out.println("You won " + numBet * 2 + " dollars!");
+			    }
+			    else { //in the case where the user wins on RED or BLACK
 				player.win(numBet);
 			    }
-			} else {
+			}
+			else { //in the case where the player loses
 			    player.lose(numBet);
 			}
 		    }
@@ -195,7 +176,7 @@ public class Woo{
 		  Blackjack
 		  ============================================*/
 		if(choice.equals("3")){
-		    Blackjack bj = new Blackjack(playerBal);
+		    Blackjack bj = new Blackjack(playerBal); //Blackjack is run on the driver in the directory
 		    if(bj.Play()){
 			player.win(bj.getBet());
 		    }
@@ -208,25 +189,25 @@ public class Woo{
 		  ============================================*/
 
 		if(choice.equals("4")){
-		    return    ;
+		    return    ; //exits the game if the user chooses choice 4
 		}
 
 		if (player.checkBal() <= 0){
 		    System.out.println("You're broke :c");
-		    return    ;
+		    return    ; //kicks the user out if they run out of money
 		}
 	    
 		System.out.print("Would you like to play again? Yes(1) No(2): ");
 		String input = Keyboard.readString();
 
-		while((!input.equals("1")) && (!input.equals("2"))){
+		while((!input.equals("1")) && (!input.equals("2"))){ //Bad cases: input is not 1 or 2
 		    System.out.print("I'm sorry, that value is invalid. Please try again: ");
 		    input = Keyboard.readString();
 		}
 		if (input.equals("2")){
-		    playAgain = false;
+		    playAgain = false; //takes the player back to the game selection menu
 		}
-		if (input.equals("1")){
+		if (input.equals("1")){ //takes the player back to the beginning of the game they previously played
 		    playAgain = true;
 		}
 	    }
